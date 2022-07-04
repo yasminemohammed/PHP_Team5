081851d65ce35126e770090500a7ef2186d77ea9
yasminemohammed <yasmine.mohamed.fathi@gmail.com>
2022-07-04 22:02:16 +0200

    pages

--- frontend_design/admin/Manual Orders.php
+++ frontend_design/admin/Manual Orders.php
@@ -4,11 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<<<<<<< HEAD
-    <link rel="stylesheet" type="text/css" href="../CSS/style.css"/>
-
-=======
->>>>>>> admin pages
     <style>
         .font{
             font-size:40px;
@@ -44,11 +39,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
-<<<<<<< HEAD
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-=======
->>>>>>> admin pages
     <section style="height:100hv ; background-color: #D9AFD9;
         background-image: linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%);">
 
@@ -92,196 +82,6 @@
 
 
 
-<<<<<<< HEAD
-          
-
-          <!-- products -->
-
-          <div class="container">
-
-<section id="cart"> 
-  <article class="product">
-    <header>
-      <a class="remove">
-        <img src="../images/tea.png">
-
-        <h3>Remove product</h3>
-      </a>
-    </header>
-
-    <div class="content">
-
-      <h1>TEA</h1>
-    </div>
-
-    <footer class="content">
-      <span class="qt-minus">-</span>
-      <span class="qt">0</span>
-      <span class="qt-plus">+</span>
-
-      <h2 class="full-price">
-        0 EGP
-      </h2>
-
-      <h2 class="price">
-        5 EGP
-      </h2>
-    </footer>
-  </article>
-
-  <article class="product">
-    <header>
-      <a class="remove">
-                <img src="../images/coffee.png">
-
-        <h3>Remove product</h3>
-      </a>
-    </header>
-
-    <div class="content">
-
-      <h1>COFFEE</h1>
-      
-    </div>
-
-    <footer class="content">
-      
-      <span class="qt-minus">-</span>
-      <span class="qt">0</span>
-      <span class="qt-plus">+</span>
-
-      <h2 class="full-price">
-        0 EGP
-      </h2>
-
-      <h2 class="price">
-        10 EGP
-      </h2>
-    </footer>
-  </article>
-
-  <article class="product">
-    <header>
-      <a class="remove">
-                <img src="../images/drink.png">
-
-        <h3>Remove product</h3>
-      </a>
-    </header>
-
-    <div class="content">
-
-      <h1>DRINK</h1>
-    </div>
-
-    <footer class="content">
-      
-      <span class="qt-minus">-</span>
-      <span class="qt">0</span>
-      <span class="qt-plus">+</span>
-
-      <h2 class="full-price">
-        0 EGP
-      </h2>
-
-      <h2 class="price">
-        15 EGP
-      </h2>
-    </footer>
-  </article>
-
-</section>
-
-</div>
-
-<footer id="site-footer">
-<div class="container clearfix">
-
-  
-  <div class="right">
-    <h1 class="total">Total: <span>0</span> EGP</h1>
-    <a class="btn">Checkout</a>
-  </div>
-
-</div>
-</footer>
-</main>
-
-  
-
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script>
-
-let check = false;
-
-function changeVal(el) {
-var qt = parseFloat(el.parent().children(".qt").html());
-var price = parseFloat(el.parent().children(".price").html());
-var eq = Math.round(price * qt * 100) / 100;
-
-el.parent().children(".full-price").html( eq + " EGP" );
-
-changeTotal();			
-}
-
-function changeTotal() {
-
-var price = 0;
-
-$(".full-price").each(function(index){
-price += parseFloat($(".full-price").eq(index).html());
-});
-
-price = Math.round(price * 100) / 100;
-var fullPrice = Math.round((price) *100) / 100;
-
-if(price == 0) {
-fullPrice = 0;
-}
-
-$(".subtotal span").html(price);
-$(".total span").html(fullPrice);
-}
-
-$(document).ready(function(){
-
-
-$(".qt-plus").click(function(){
-$(this).parent().children(".qt").html(parseInt($(this).parent().children(".qt").html()) + 1);
-
-$(this).parent().children(".full-price").addClass("added");
-
-var el = $(this);
-window.setTimeout(function(){el.parent().children(".full-price").removeClass("added"); changeVal(el);}, 150);
-});
-
-$(".qt-minus").click(function(){
-
-child = $(this).parent().children(".qt");
-
-if(parseInt(child.html()) > 1) {
-  child.html(parseInt(child.html()) - 1);
-}
-
-$(this).parent().children(".full-price").addClass("minused");
-
-var el = $(this);
-window.setTimeout(function(){el.parent().children(".full-price").removeClass("minused"); changeVal(el);}, 150);
-});
-
-window.setTimeout(function(){$(".is-open").removeClass("is-open")}, 1200);
-
-$(".btn").click(function(){
-check = true;
-$(".remove").click();
-});
-});
-</script> 
-
-
-          
-
-=======
 
 
 
@@ -486,7 +286,6 @@ $(".remove").click();
 
 
       </script>
->>>>>>> admin pages
 
 </body>
 </html>
--- frontend_design/users/home.php
+++ frontend_design/users/home.php
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-<html>
-=======
 <!doctype html>
 <html lang="en">
->>>>>>> admin pages
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<<<<<<< HEAD
-    <link rel="stylesheet" type="text/css" href="../CSS/style.css"/>
-
-=======
->>>>>>> admin pages
     <style>
         .font{
             font-size:40px;
@@ -29,11 +20,7 @@
           text-decoration : none;
         }
         .align{
-<<<<<<< HEAD
-        margin-left:700px;
-=======
-        margin-left:900px;
->>>>>>> admin pages
+        margin-left: 900px;
         }
       .img{
         width:50%;
@@ -45,10 +32,6 @@
       .mar{
         margin-top:18px;
       }
-<<<<<<< HEAD
-    
-=======
->>>>>>> admin pages
     </style>
   </head>
 <body>
@@ -56,24 +39,11 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
-<<<<<<< HEAD
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <section style="height:100hv ; background-color: #D9AFD9;
+    <section  style=" height:100%;background-color: #D9AFD9;
         background-image: linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%);">
 
  <!-- Navbar -->
  <nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong">
-=======
-    <section  style=" height:100%;background-color: #D9AFD9;
-        background-image: linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%);">
-
-
-        <!-- navbar -->
-
-
-        <nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong">
->>>>>>> admin pages
                 <div class="container-fluid ">
                     <a class="navbar-brand" href="#">Cafetria</a>
                     <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
@@ -84,27 +54,16 @@
                     <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 align">
 
-<<<<<<< HEAD
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                        </li>
-
-                
-                        <li class="nav-item">
-                        <a class="nav-link" href="myorders.php">My Orders</a>
-                        </li>
-                       
-=======
                         <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
 
+                      
                         <li class="nav-item">
                         <a class="nav-link" href="#">My Orders</a>
                         </li>
 
                      
->>>>>>> admin pages
                     </ul>
                     </div>
                 
@@ -112,200 +71,12 @@
                 </nav>
 
 
-<<<<<<< HEAD
-
-          
-
-          <!-- products -->
-
-          <div class="container">
-
-<section id="cart"> 
-  <article class="product">
-    <header>
-      <a class="remove">
-        <img src="../images/tea.png">
-
-        <h3>Remove product</h3>
-      </a>
-    </header>
-
-    <div class="content">
-
-      <h1>TEA</h1>
-    </div>
-
-    <footer class="content">
-      <span class="qt-minus">-</span>
-      <span class="qt">0</span>
-      <span class="qt-plus">+</span>
-
-      <h2 class="full-price">
-        0 EGP
-      </h2>
-
-      <h2 class="price">
-        5 EGP
-      </h2>
-    </footer>
-  </article>
-
-  <article class="product">
-    <header>
-      <a class="remove">
-                <img src="../images/coffee.png">
-
-        <h3>Remove product</h3>
-      </a>
-    </header>
-
-    <div class="content">
-
-      <h1>COFFEE</h1>
-      
-    </div>
-
-    <footer class="content">
-      
-      <span class="qt-minus">-</span>
-      <span class="qt">0</span>
-      <span class="qt-plus">+</span>
-
-      <h2 class="full-price">
-        0 EGP
-      </h2>
-
-      <h2 class="price">
-        10 EGP
-      </h2>
-    </footer>
-  </article>
-
-  <article class="product">
-    <header>
-      <a class="remove">
-                <img src="../images/drink.png">
-
-        <h3>Remove product</h3>
-      </a>
-    </header>
-
-    <div class="content">
-
-      <h1>DRINK</h1>
-    </div>
-
-    <footer class="content">
-      
-      <span class="qt-minus">-</span>
-      <span class="qt">0</span>
-      <span class="qt-plus">+</span>
-
-      <h2 class="full-price">
-        0 EGP
-      </h2>
-
-      <h2 class="price">
-        15 EGP
-      </h2>
-    </footer>
-  </article>
-
-</section>
-
-</div>
-
-<footer id="site-footer">
-<div class="container clearfix">
-
-  
-  <div class="right">
-    <h1 class="total">Total: <span>0</span> EGP</h1>
-    <a class="btn">Checkout</a>
-  </div>
-
-</div>
-</footer>
-</main>
-
-  
-
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script>
-
-let check = false;
-
-function changeVal(el) {
-var qt = parseFloat(el.parent().children(".qt").html());
-var price = parseFloat(el.parent().children(".price").html());
-var eq = Math.round(price * qt * 100) / 100;
-
-el.parent().children(".full-price").html( eq + " EGP" );
-
-changeTotal();			
-}
-
-function changeTotal() {
-
-var price = 0;
-
-$(".full-price").each(function(index){
-price += parseFloat($(".full-price").eq(index).html());
-});
-
-price = Math.round(price * 100) / 100;
-var fullPrice = Math.round((price) *100) / 100;
-
-if(price == 0) {
-fullPrice = 0;
-}
-
-$(".subtotal span").html(price);
-$(".total span").html(fullPrice);
-}
-
-$(document).ready(function(){
 
 
-$(".qt-plus").click(function(){
-$(this).parent().children(".qt").html(parseInt($(this).parent().children(".qt").html()) + 1);
-
-$(this).parent().children(".full-price").addClass("added");
-
-var el = $(this);
-window.setTimeout(function(){el.parent().children(".full-price").removeClass("added"); changeVal(el);}, 150);
-});
-
-$(".qt-minus").click(function(){
-
-child = $(this).parent().children(".qt");
-
-if(parseInt(child.html()) > 1) {
-  child.html(parseInt(child.html()) - 1);
-}
-
-$(this).parent().children(".full-price").addClass("minused");
-
-var el = $(this);
-window.setTimeout(function(){el.parent().children(".full-price").removeClass("minused"); changeVal(el);}, 150);
-});
-
-window.setTimeout(function(){$(".is-open").removeClass("is-open")}, 1200);
-
-$(".btn").click(function(){
-check = true;
-$(".remove").click();
-});
-});
-</script> 
 
 
-          
 
 
-</body>
-</html>
-=======
                 <!-- home -->
 
 
@@ -313,7 +84,7 @@ $(".remove").click();
 
    <div class="container">
 
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-5">
  
   <div class="col mb-4">
     <div class="card">
@@ -323,7 +94,7 @@ $(".remove").click();
         <p class="card-text text-center">
 
        <div class="price">
-      <p>Price : $<span class="item_price">7</span></p>
+      <p>Price : $<span class="item_price">5</span></p>
       <p>Subtotal : <b>$<span class="total">0</span></b></p>
     </div>
     <div class="quantity">
@@ -363,7 +134,7 @@ $(".remove").click();
         <p class="card-text text-center">
 
       <div class="price">
-      <p>Price : $<span class="item_price">7</span></p>
+      <p>Price : $<span class="item_price">8</span></p>
       <p>Subtotal : <b>$<span class="total">0</span></b></p>
       </div>
       <div class="quantity">
@@ -383,7 +154,7 @@ $(".remove").click();
         <p class="card-text text-center">
 
       <div class="price">
-      <p>Price : $<span class="item_price">7</span></p>
+      <p>Price : $<span class="item_price">10</span></p>
       <p>Subtotal : <b>$<span class="total">0</span></b></p>
       </div>
       <div class="quantity">
@@ -403,7 +174,7 @@ $(".remove").click();
         <p class="card-text text-center">
 
         <div class="price">
-        <p>Price : $<span class="item_price">7</span></p>
+        <p>Price : $<span class="item_price">15</span></p>
         <p>Subtotal : <b>$<span class="total">0</span></b></p>
         </div>
         <div class="quantity">
@@ -470,7 +241,6 @@ $(".remove").click();
   </div>
   <div class="card-body">
     <p class="card-text">
-    <b>$<span class="summ">0</span></b>
 
     </p>
     <a href="#" class="btn btn-primary">Confirm</a>
@@ -508,9 +278,7 @@ $(".remove").click();
 
 
 
-
-
       </script>
 
 </body>
->>>>>>> admin pages
+</html>
